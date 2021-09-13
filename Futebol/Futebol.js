4d5d874f38edc51df5df5290fd1e54d00a1f24be
Isaac Rocha <isaacgeazy@gmail.com>
2021-09-13 12:16:18 -0300

    JScript n ta pronto, mas concluido pra visualização

---
+++ Futebol/Futebol.js
@@ -0,0 +1,10 @@
+function Conferir(){
+    let result = window.document.getElementById('Result');
+
+    let opcao1 = window.document.querySelector('input#cinco');
+    let opcao2 = window.document.querySelector('input#dois');
+    let opcao3 = window.document.querySelector('input#tres');
+    let opcao4 = window.document.querySelector('input#dez');
+    let opcao5 = window.document.querySelector('input#um');
+
+}
